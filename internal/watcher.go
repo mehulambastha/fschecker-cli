b51cb395c222dd6ed6ad3f12745eed53bfe5216b
mehulambastha <mehul.213amb@gmail.com>
2024-09-25 13:41:20 +0530

    Setup differentiating between type of event sand triggering responses based on that.

--- internal/watcher.go
+++ internal/watcher.go
@@ -1,15 +1,42 @@
 package internal
 
 import (
-	_ "fmt"
-	"github.com/fsnotify/fsnotify"
+	"fmt"
 	"log"
+	"slices"
+	"strings"
+	"time"
+
+	"github.com/fsnotify/fsnotify"
 )
+
+func IsMediaFile(fileName string) bool {
+	splitted := strings.Split(fileName, ".")
+	extension := strings.TrimSpace(strings.ToLower(splitted[len(splitted)-1]))
+
+	mediaExtensions := []string{
+		"jpg", "jpeg", "png", "gif", "bmp", "tiff", "webp", // Image formats
+		"mp3", "wav", "flac", "aac", "ogg", "m4a", // Audio formats
+		"mp4", "avi", "mkv", "mov", "wmv", "webm", "flv", // Video formats
+		"pdf", "epub", "mobi", "azw3", "doc", "docx", // Document formats
+		"xls", "xlsx", "ppt", "pptx", "odt", "ods", "odp", // Office formats
+		"txt", "md", "rtf", "log", // Text formats
+		"zip", "rar", "7z", "tar", "gz", "bz2", "xz", // Compressed formats
+		"iso", "img", "bin", // Disk image formats
+		"svg", "psd", "ai", "eps", "pdf", "xcf", // Design/vector formats
+	}
+
+	if slices.Contains(mediaExtensions, extension) {
+		return true
+	}
+	return false
+}
 
 func WatchDir(dir string) {
 	watcher, err := fsnotify.NewWatcher()
 	defer watcher.Close()
 
+	debouncingTimerState := make(map[string]time.Time)
 	go func() {
 		for {
 			select {
@@ -17,12 +44,24 @@ func WatchDir(dir string) {
 				if !ok {
 					return
 				}
-				log.Println("event: ", event)
-
 				if event.Has(fsnotify.Write) {
-					log.Println("A file was modified at: ", event.Name)
+					debouncingTimerState[event.Name] = time.Now()
+
+					go func(fileName string) {
+						time.Sleep(150 * time.Millisecond)
+						if time.Since(debouncingTimerState[fileName]) >= 150*time.Millisecond {
+							addAndCommit(fileName)
+						}
+					}(event.Name)
+
 				} else if event.Has(fsnotify.Create) {
-					log.Println("A new file was created: ", event.Name)
+
+					// Checking if this is a media file, and that if it needs to be added as soon as it is created.
+					if IsMediaFile(event.Name) {
+						addAndCommit(event.Name)
+					} else {
+						continue
+					}
 				}
 			case err, ok := <-watcher.Errors:
 				if !ok {
@@ -39,4 +78,8 @@ func WatchDir(dir string) {
 	}
 
 	<-make(chan struct{})
+}
+
+func addAndCommit(fileName string) {
+	fmt.Println("adding and commtting..", fileName)
 }
